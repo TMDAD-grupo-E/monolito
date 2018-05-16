e3e2e38e61b0853c68b1c07cf6f657ea43386149
diegoalvarezg <554716@unizar.es>
2018-05-16 02:46:19 +0200

    Subject: Modified to include streaming - Back End

--- src/main/java/es/unizar/tmdad/lab2/controller/SearchController.java
+++ src/main/java/es/unizar/tmdad/lab2/controller/SearchController.java
@@ -1,18 +1,14 @@
 package es.unizar.tmdad.lab2.controller;
 
+import es.unizar.tmdad.lab2.service.TwitterLookupService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.social.UncategorizedApiException;
 import org.springframework.social.twitter.api.SearchResults;
 import org.springframework.social.twitter.api.Trends;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
-
-import es.unizar.tmdad.lab2.service.TwitterLookupService;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class SearchController {
@@ -20,10 +16,16 @@ public class SearchController {
 	@Autowired
 	TwitterLookupService twitter;
 
-	@RequestMapping("/search")
+	@MessageMapping("/search")
+	public void search(String q) {
+		twitter.search(q);
+	}
+
+	/*@RequestMapping("/search")
 	public SearchResults search(@RequestParam("q") String q, Model m) {
 		return twitter.search(q);
-	}
+	}*/
+
 
 	@RequestMapping("/trends")
 	public Trends trends(@RequestParam("c") String c, Model m) {
@@ -32,7 +34,7 @@ public class SearchController {
 
 	@ResponseStatus(value = HttpStatus.BAD_REQUEST)
 	@ExceptionHandler(UncategorizedApiException.class)
-	public SearchResults handleUncategorizedApiException(Model m) {
+	public SearchResults handleUncategorizedApiException() {
 		return twitter.emptyAnswer();
 	}
 
--- src/main/java/es/unizar/tmdad/lab2/service/TwitterLookupService.java
+++ src/main/java/es/unizar/tmdad/lab2/service/TwitterLookupService.java
@@ -1,19 +1,22 @@
 package es.unizar.tmdad.lab2.service;
 
-import java.util.Collections;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.social.twitter.api.SearchMetadata;
-import org.springframework.social.twitter.api.SearchResults;
-import org.springframework.social.twitter.api.Trends;
-import org.springframework.social.twitter.api.Twitter;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.social.twitter.api.*;
 import org.springframework.social.twitter.api.impl.TwitterTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 
 import es.unizar.tmdad.lab2.utils.CountryAdapter;
 
 @Service
 public class TwitterLookupService {
+
 	@Value("${twitter.consumerKey}")
 	private String consumerKey;
 
@@ -26,11 +29,40 @@ public class TwitterLookupService {
 	@Value("${twitter.accessTokenSecret}")
 	private String accessTokenSecret;
 
+	/*private String consumerKey = System.getenv("consumerKey");
+
+	private String consumerSecret = System.getenv("consumerSecret");
+
+	private String accessToken = System.getenv("accessToken");
+
+	private String accessTokenSecret = System.getenv("accessTokenSecret");*/
+
 	CountryAdapter adapter = new CountryAdapter();
 
-	public SearchResults search(String query) {
+	/*public SearchResults search(String query) {
 		Twitter twitter = new TwitterTemplate(consumerKey, consumerSecret, accessToken, accessTokenSecret);
 		return twitter.searchOperations().search(query);
+	}*/
+
+
+	@Autowired
+	private SimpMessageSendingOperations messagingTemplate;
+
+	private ArrayList<String> queries = new ArrayList<>();
+	List<StreamListener> twitterList = new ArrayList<StreamListener>();
+	Stream s;
+
+
+	public void search(String q) {
+
+		Twitter twitter = new TwitterTemplate(consumerKey, consumerSecret, accessToken, accessTokenSecret);
+		if (queries.size() >= 10) {
+			queries.remove(0);
+			twitterList.remove(0);
+		}
+		twitterList.add(new SimpleStreamListener(messagingTemplate, q));
+		s = twitter.streamingOperations().filter(q, twitterList);
+
 	}
 
 	public Trends trends(String woeid) {
@@ -39,7 +71,7 @@ public class TwitterLookupService {
 	}
 
 	public SearchResults emptyAnswer() {
-		return new SearchResults(Collections.emptyList(), new SearchMetadata(0, 0));
+		return new SearchResults(Collections.emptyList(), new SearchMetadata(0, 0 ));
 	}
 
 	public Trends trendsEmptyAnswer() {
